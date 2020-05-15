c5ef29ba0e1b314cedbb2453bd21878c59851d24
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-05-15 19:38:41 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200515193500) do
+ActiveRecord::Schema.define(version: 20200515193810) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.decimal "balance"
+  end
 
 end
